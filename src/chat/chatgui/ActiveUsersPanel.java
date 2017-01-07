fe47829f6d36b8482ada94c0803bd8748026c924
siorekoskar <siorekoskar@gmail.com>
2017-01-07 17:55:40 +0100

    added userPopupMenu to ActiveUsersPanel

--- src/chat/chatgui/ActiveUsersPanel.java
+++ src/chat/chatgui/ActiveUsersPanel.java
@@ -2,18 +2,44 @@ package chat.chatgui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 /**
  * Created by Oskar on 07/01/2017.
  */
 public class ActiveUsersPanel extends JPanel {
     private JList activeUsersPanel;
+    private JPopupMenu popupUserMenu;
+
+    private int getRow(Point point){
+        return activeUsersPanel.locationToIndex(point);
+    }
 
     public ActiveUsersPanel(){
+
+
 
         DefaultListModel activeUsersModel = new DefaultListModel();
         activeUsersPanel = new JList(activeUsersModel);
         activeUsersPanel.setBorder(BorderFactory.createTitledBorder("Active users:"));
+
+
+        ////////////////TEMP//////////////////
+        popupUserMenu = new JPopupMenu();
+        popupUserMenu.add(new JMenuItem("Invite"));
+        popupUserMenu.add(new JMenuItem("Kick"));
+        popupUserMenu.add(new JMenuItem("Whisper"));
+        activeUsersPanel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                if (SwingUtilities.isRightMouseButton(e)){
+                    activeUsersPanel.setSelectedIndex(getRow(e.getPoint()));
+                    popupUserMenu.show(activeUsersPanel, e.getX(), e.getY());
+                }
+            }
+        });
+        ////////////////TEMP//////////////////
 
         for (int i = 0; i < 40; i++) {
             activeUsersModel.addElement("User nr  " + i);
