7fc78738587eeaf3c7a03d490d7dae838964eac0
siorekoskar <siorekoskar@gmail.com>
2017-01-08 20:56:44 +0100

    Database - added connections and saving to database

--- src/chat/TestDatabase.java
+++ src/chat/TestDatabase.java
@@ -3,6 +3,8 @@ package chat;
 import chat.model.Database;
 import chat.model.User;
 
+import java.sql.SQLException;
+
 /**
  * Created by Oskar on 08/01/2017.
  */
@@ -12,8 +14,22 @@ public class TestDatabase {
 
         Database db = new Database();
 
+        try{
+            db.connect();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
         db.addUser(new User("okularki111", "mama123"));
         db.addUser(new User("michael", "mama123"));
+
+        try {
+            db.save();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        db.disconnect();
 
         System.out.println("Test end");
     }
--- src/chat/model/Database.java
+++ src/chat/model/Database.java
@@ -1,6 +1,6 @@
 package chat.model;
 
-import java.sql.Connection;
+import java.sql.*;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
@@ -36,13 +36,79 @@ public class Database{
     ///////////DO
     public void connect() throws Exception{
 
+        if(conn!=null){
+            return;
+        }
+
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch(ClassNotFoundException e){
+            throw new Exception("Driver not found");
+        }
+
+        String url = "jdbc:mysql://localhost:3306/user";
+
+        conn = DriverManager.getConnection(url, "root", "shihtzu1");
+
+        System.out.println("Connected: " + conn);
     }
 
     public void disconnect(){
-
+        if (conn != null){
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                System.out.println("Cant close connection");
+            }
+        }
     }
 
-    public void save(){
+    public void save() throws SQLException{
+
+        String checkSql = "Select count(*) as count from user where id=?";
+        PreparedStatement checkStmt = conn.prepareStatement(checkSql);
+
+        String insertSql = "insert into user (login, password) values (?,?)";
+        PreparedStatement insertStmt = conn.prepareStatement(insertSql);
+
+        String updateSql = "update user set login=?, password=? where id = ?";
+        PreparedStatement updateStmt = conn.prepareStatement(updateSql);
+
+        for(User user: users){
+            int id = user.getId();
+            String login = user.getLogin();
+            String password = user.getPass();
+
+            checkStmt.setInt(1, id);
+
+            ResultSet checkResult = checkStmt.executeQuery();
+            checkResult.next();
+
+            int count = checkResult.getInt(1);
+
+            if(count == 0){
+                System.out.println("Inserting person with ID " + id);
+
+                int col = 1;
+                insertStmt.setInt(col, id);
+                insertStmt.setString(col++, login);
+                insertStmt.setString(col++, password);
+
+                insertStmt.executeUpdate();
+            }
+            else {
+                System.out.println("Updating person with ID" + id);
+
+                int col = 1;
+                updateStmt.setString(col++, login);
+                updateStmt.setString(col++, password);
+                updateStmt.setInt(col++, id);
+
+                updateStmt.executeUpdate();
+            }
+
+
+        }
 
     }
 
